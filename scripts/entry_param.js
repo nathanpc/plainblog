4d35c0a3eb6733620c89aff356a0171f6cbfa0b4
Nathan Campos <eeepc904@gmail.com>
2013-02-12 17:14:43 -0200

    Cleaned that old mess. That code gives me nightmares. I'll rewrite it from scratch

--- scripts/entry_param.js
+++
@@ -1,6 +0,0 @@
-// entry_param.js
-// Captures the url parameter and parse it
-
-function getParamByName(name) {
-	return decodeURIComponent((RegExp(name + "=" + "(.+?)(&|$)").exec(location.search)||[,null])[1]);
-}
